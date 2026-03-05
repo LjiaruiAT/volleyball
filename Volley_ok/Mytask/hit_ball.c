a298fa02fc67209afd8556373df4bd2de52fd2f5
Liu Jiarui <3499682469@qq.com>
2026-03-05 17:20:46 +0800

    加入三次多项式，修改控制逻辑

--- Volley_ok/Mytask/hit_ball.c
+++ Volley_ok/Mytask/hit_ball.c
@@ -1,4 +1,8 @@
 #include "hit_ball.h"
+CubicParam_t cubic;       // 三次多项式参数
+TrajectoryState_t state;  // 存储当前位置、速度、加速度
+int take = 0;
+
 uint32_t error_cnt = 0;
 uint32_t last_error_time = 0;
 ErrorStats_t error_stats = {0};
@@ -23,6 +27,12 @@ TaskHandle_t Hit_Task_Handle;
 
 void Hit_Task(void *pvParameters)
 {
+	
+int16_t cur_motor_pos = GoMotorRecv(&let_fly.go_volleyball); // 获取当前位置
+float cur_pos_rad = cur_motor_pos * 2.0f * M_PI / 32768.0f; // 转成弧度
+
+
+	
 rm3508.pos_pid_3508.Kp =0.0f;
 rm3508.pos_pid_3508.Ki =0.0f;
 rm3508.pos_pid_3508.Kd =0.0f;
@@ -37,20 +47,44 @@ rm3508.vel_pid_3508.output_limit = 10000.0f;
 TickType_t Last_wake_time = xTaskGetTickCount();
 for(;;)
 	{
+		if (take == 0)
+		{
+			// 设置三次多项式轨迹
+         Cubic_SetTrajectory(&cubic, 
+                    cur_pos_rad, 0.0f,       // 当前位姿和速度
+                    let_fly.exp.exp_pos, 0.0f, // 目标位姿和目标速度
+                    0.2f, HAL_GetTick());    // 持续时间 0.2s (可调)，当前时间
+			
+		}
+		if (take == 1)
+		{
+			
+			uint32_t now = HAL_GetTick();
+
+    // 获取三次多项式轨迹状态
+    Cubic_GetFullState(&cubic, now, &state);
+
+    // 用 state.pos 和 state.vel 下发电机指令
+    GoMotorSend(&let_fly.go_volleyball,
+                let_fly.exp.exp_tor,  // 保持你的期望力矩
+                state.vel,            // 使用轨迹速度
+                state.pos,            // 使用轨迹位置
+                let_fly.exp.exp_kp,
+                let_fly.exp.exp_kd);
+			
+		}
 //	PID_Control2(rm3508.motor_3508.motor.MchanicalAngle,exp_3508.exp_pos,&rm3508.pos_pid_3508);
 //  PID_Control2(rm3508.motor_3508.motor.Speed,rm3508.pos_pid_3508.pid_out,&rm3508.vel_pid_3508);
 //  can_send_buf[0]=(int16_t)rm3508.pos_pid_3508.pid_out;
 //  MotorSend(&hcan1,0x200,can_send_buf);
-	//HAL_Delay(200);//电机松手，排球自由落体，这时宇树电机击球
-	GoMotorSend(&let_fly.go_volleyball,let_fly.exp.exp_tor,let_fly.exp.exp_vel,let_fly.exp.exp_pos,let_fly.exp.exp_kp,let_fly.exp.exp_kd);
-
+//  HAL_Delay(200);//电机松手，排球自由落体，这时宇树电机击球
 //	HAL_Delay(5000);
 	//GoMotorSend这里宇树电机复位
 //	HAL_Delay(1000);
 	//MotorSend这里3508电机复位
 		int ret = GoMotorRecv(&let_fly.go_volleyball);
 
-	vTaskDelayUntil(&Last_wake_time, pdMS_TO_TICKS(2));
+	vTaskDelayUntil(&Last_wake_time, pdMS_TO_TICKS(5));
   }
 }
 void HAL_UART_TxCpltCallback(UART_HandleTypeDef *huart)
